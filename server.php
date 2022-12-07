1bfbaead8f55bc971dc539c3f199a6bf557bc260
Thunderbird93 <antircristian@gmail.com>
2022-12-07 16:10:34 +0100

    fine

--- server.php
+++ server.php
@@ -1,8 +1,9 @@
 <?php
-$stringa = file_get_contents('dischi.json');
-$dischi = json_decode($stringa, true);
+$string = file_get_contents('dischi.json');
+$dischi = json_decode($string, true);
 
-//var_dump($dischi);
+var_dump($dischi);
+
 
 header('Content-Type: application/json');
 echo json_encode($dischi);
