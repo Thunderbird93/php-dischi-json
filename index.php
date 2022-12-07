66ac21220bd12c6275453d6a0da60e7e1f0a688f
Thunderbird93 <antircristian@gmail.com>
2022-12-07 16:09:58 +0100

    [add] html

--- index.php
+++ index.php
@@ -3,19 +3,40 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 
 <body>
-  <div id="app">
-  </div>
-  <script src="./main.js"></script>
+    <div id="app">
+
+        <header>
+            <div class="container" id="header-container">
+                <div class="header-left">
+                    <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div id="discs-container">
+                    <div v-for="disco in dischi"
+                    class="disco">
+                        <img :src="disco.poster" :alt="disco.title" />
+                        <h3>{{ disco.title }}</h3>
+                        <small>{{ disco.author }}</small>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    </div>
+    <script src="./main.js"></script>
 </body>
+
 </html>
